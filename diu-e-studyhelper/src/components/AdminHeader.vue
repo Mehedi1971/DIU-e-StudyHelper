8df7d6c42282b51e38546be79489c27d73af6752
shihab <mh.shihab001@gmail.com>
2022-03-29 01:59:26 +0600

    new

--- diu-e-studyhelper/src/components/AdminHeader.vue
+++ diu-e-studyhelper/src/components/AdminHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <!-- <div v-if="!user">
+    <div>
       <nav
         class="flex flex-wrap items-center justify-between px-2 py-6 bg-white mb-3 fixed top-0 inset-x-0 z-50"
       >
@@ -10,82 +10,13 @@
           <div
             class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
           >
-            <div class="flex items-center cursor-pointer">
+            <div class="flex items-center cursor-pointer pl-6 pr-3 fixed">
               <router-link to="/" class="hover:no-underline">
-                <img src="../assets/diulogo04.svg" alt="" class="h-11 w-12"
-              /></router-link> -->
-
-    <!-- <span class="text-green-500 text-xl mr-1">
-                <i class="bi bi-stack"></i>
-              </span>
-              <router-link to="/" class="hover:no-underline">
-                <h1 class="text-2xl font-bold">
-                  <span class="text-blue-500">e</span
-                  ><span class="text-green-500">-StudyHelper</span>
-                </h1></router-link
-              > -->
-    <!-- </div>
-            <button
-              class="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-              type="button"
-              @click="toggleNavbar"
-            >
-              <i class="bi bi-list"></i>
-            </button>
-          </div>
-          <div
-            v-bind:class="{ hidden: !showMenu, flex: showMenu }"
-            class="lg:flex lg:flex-grow items-center"
-          >
-            <ul class="flex flex-col lg:flex-row list-none ml-auto">
-              <li>
-                <router-link
-                  to="/Login"
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75 hover:no-underline"
-                >
-                  <i
-                    class="fab fa-pinterest text-lg leading-lg text-black opacity-75"
-                  /><span class="ml-2">Login</span>
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  to="/SignUp"
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75 hover:no-underline"
-                >
-                  <i
-                    class="fab fa-pinterest text-lg leading-lg text-black opacity-75"
-                  /><span class="ml-2">Sign Up</span>
-                </router-link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <div><router-view /></div>
-    </div> -->
-
-    <!-- 2nd -->
-    <div v-if="!user">
-      <nav
-        class="flex flex-wrap items-center justify-between px-2 py-6 bg-white mb-3 fixed top-0 inset-x-0 z-50"
-      >
-        <div
-          class="container px-4 max-w-6xl mx-auto flex flex-wrap items-center justify-between"
-        >
-          <div
-            class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
-          >
-            <div class="flex items-center cursor-pointer">
-              <span class="text-green-500 text-xl mr-1">
-                <i class="bi bi-stack"></i>
-              </span>
-              <router-link to="/" class="hover:no-underline">
-                <h1 class="text-2xl font-bold">
-                  <span class="text-blue-500">e</span
-                  ><span class="text-green-500">-StudyHelper</span>
-                </h1></router-link
-              >
+                <img
+                  src="../assets/diu05.png"
+                  alt=""
+                  class="h-0 w-0 lg:h-11 lg:w-12"
+              /></router-link>
             </div>
             <button
               class="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -123,7 +54,7 @@
               </li>
               <li>
                 <router-link
-                  to="#"
+                  to="/adminCourseMaterials"
                   class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75 hover:no-underline"
                 >
                   <i
@@ -142,26 +73,6 @@
                   /><span class="ml-2">Logout</span>
                 </router-link>
               </li>
-              <!-- <li>
-                <router-link
-                  to="/Login"
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75 hover:no-underline"
-                >
-                  <i
-                    class="fab fa-pinterest text-lg leading-lg text-black opacity-75"
-                  /><span class="ml-2">Login</span>
-                </router-link>
-              </li>
-              <li>
-                <router-link
-                  to="/SignUp"
-                  class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-black hover:opacity-75 hover:no-underline"
-                >
-                  <i
-                    class="fab fa-pinterest text-lg leading-lg text-black opacity-75"
-                  /><span class="ml-2">Sign Up</span>
-                </router-link>
-              </li> -->
             </ul>
           </div>
         </div>
