075d20f250952756b7cc54f1708a743d2d4ce997
shihab <mh.shihab001@gmail.com>
2022-05-15 19:19:48 +0600

    update

--- diu-e-studyhelper/src/axios.js
+++ diu-e-studyhelper/src/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
-axios.defaults.baseURL =
-  'https://estudyhelperapi.herokuapp.com/api/eStudyHelper/'
+axios.defaults.baseURL = 'http://localhost:3000/api/eStudyHelper/'
 axios.defaults.headers.common['jwt'] = localStorage.getItem('jwt')
 axios.defaults.headers.common['token'] = localStorage.getItem('token')
